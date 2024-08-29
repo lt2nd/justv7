7772bfc4614f860d5948973e1f3a32d126e3475b
lt2nd <bolelole207@gmail.com>
2024-08-29 23:52:43 +0200

    likes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,11 +3,14 @@ import { Alert, FlatList, Image, Text, View } from "react-native"
 import posts from "~/assets/data/posts.json"
 import PostListItem from "~/src/components/PostListItem";
 import { supabase } from "~/src/lib/supabase";
+import { useAuth } from "../providers/AuthProvider";
 
 export default function FeedScreen() {
 
    const [posts, setPosts] = useState([] as any);
    const [loading, setLoading] = useState(false);
+
+   const { user } = useAuth();
 
    useEffect(() => {
       fetchPosts();
@@ -17,7 +20,8 @@ export default function FeedScreen() {
       setLoading(true);
       let { data, error } = await supabase
       .from('posts')
-      .select('*, user: profiles(*)')
+      .select('*, user:profiles(*), my_likes:likes(*), likes(count)')
+      .eq('my_likes.user_id', user.id)
       .order('created_at', {ascending: false});
       
       if(error) {
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -12,8 +12,58 @@ import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
 import { cld } from "../lib/cloudinary";
 import { ResizeMode, Video } from "expo-av";
 import PostContent from "./PostContent";
+import { useEffect, useState } from "react";
+import { useAuth } from "../app/providers/AuthProvider";
+import { supabase } from "../lib/supabase";
 
 export default function PostListItem({ post }: any) {
+
+    const { user } = useAuth();
+
+    const [isLiked, setIsLiked] = useState(false);
+    const [likeRecord, setLikeRecord] = useState(null);
+
+    useEffect(() => {
+        if (post.my_likes.length > 0) {
+            setLikeRecord(post.my_likes[0]);
+            setIsLiked(true);
+        }
+    }, [post.my_likes]);
+
+    useEffect(() => {
+        if (isLiked) {
+            saveLike();
+        } else {
+            deleteLike();
+        }
+    }, [isLiked]);
+
+    const saveLike = async () => {
+        if (likeRecord) {
+            return;
+        }
+        const { data } = await supabase
+            .from('likes')
+            .insert([{ user_id: user?.id, post_id: post.id }])
+            .select();
+
+        // send notification to the owner of that post
+        //sendLikeNotification(data[0]);
+
+        setLikeRecord(data[0]);
+    };
+
+    const deleteLike = async () => {
+        if (likeRecord) {
+            const { error } = await supabase
+                .from('likes')
+                .delete()
+                .eq('id', likeRecord.id);
+            if (!error) {
+                setLikeRecord(null);
+            }
+        }
+    };
 
     const avatar = cld.image(post.user.avatar_url || 'default_qxvvyb');
     avatar.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face())));
@@ -32,12 +82,31 @@ export default function PostListItem({ post }: any) {
                     {post.user.username || 'New User'}
                 </Text>
             </View>
-            
+
             {/* Content */}
+            <View className="px-3 gap-1">
+                
+                <Text>
+                    <Text className="font-semibold">
+                        {post.user.username || 'New user'}{' '}
+                    </Text>
+                    {post.caption}
+                </Text>
+            </View>
             <PostContent post={post} />
-            
+
             <View className="flex-row  justify-between p-3" >
-                <AntDesign name="like2" size={24} />
+                <View >
+                <AntDesign
+                    onPress={() => setIsLiked(!isLiked)}
+                    name={isLiked ? "like1" : "like2"}
+                    size={24}
+                    color={isLiked ? 'navy' : 'black'}
+                />
+                 <Text className="font-semibold">
+          {post.likes?.[0]?.count || 0} likes
+        </Text>
+                </View>
                 <FontAwesome name="comment-o" size={24} />
                 <Feather name="send" size={24} />
                 <Ionicons name="save-outline" size={24} />
